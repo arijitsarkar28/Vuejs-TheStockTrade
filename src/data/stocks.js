8ee7dd08ce97007561fcea6fd21ee5f696c2e501
Arijit <tarijit.1900@gmail.com>
2020-08-02 20:44:33 +0530

    first commit

---
+++ src/data/stocks.js
@@ -0,0 +1,8 @@
+export default [
+    {id: 1, name:'Bwm', price: 110},
+                {id: 2, name:'Fiat', price: 111},
+                {id: 3, name:'Ford', price: 140},
+                {id: 4, name:'Maruti', price: 115},
+                {id: 5, name:'Audi', price: 90},
+                {id: 6, name:'Tata', price: 100}
+]
